28c3da4d7717aaf4cd01bf38a30c522df95082b1
Oscar Alejandro <oscarale930719@gmail.com>
2019-02-12 00:18:23 -0600

    Create 04directed_acyclic_graph.py

---
+++ Flujo_Redes_T1/04directed_acyclic_graph.py
@@ -0,0 +1,11 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G=nx.DiGraph()                                  #Create an empty directed graph
+G.add_node("X")
+G.add_nodes_from(["R1","R2","R3","R4"])
+
+G.add_edges_from([("X","R1"),("X","R2"),("X","R3"),("X","R4")])
+nx.draw(G,with_labels=True)
+plt.savefig("Graph04.eps", format="EPS")
+plt.show()
