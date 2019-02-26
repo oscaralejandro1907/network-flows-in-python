84c2073213fddc08272dfaff6df40b41914a6570
Oscar Alejandro <oscarale930719@gmail.com>
2019-02-26 02:27:04 -0600

    Create graph02ucg.py

---
+++ Flujo_Redes_T2/graph02ucg.py
@@ -0,0 +1,11 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G=nx.Graph()
+G.add_nodes_from(['A','B','C','D','E'])
+
+G.add_edges_from([('A','B'),('B','C'),('C','D'),('D','E'),('E','A')])
+
+nx.draw(G,pos=nx.circular_layout(G),with_labels=True)  #Draw the Graph named G with the circular layout
+plt.savefig("Graph02.eps", format="EPS")
+plt.show()
